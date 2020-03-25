09595cb2833b3bf2b11524076927640c25d0d25c
Christopher Rogers <crogers@spartaglobal.com>
2020-03-25 13:31:14 +0000

    asdsa

--- main.tf
+++ main.tf
@@ -20,6 +20,29 @@ data "template_file" "db2_init" {
   template = "${file("./scripts/db/init2.sh.tpl")}"
 }
 
+data "aws_ami" "app" {
+  executable_users = ["self"]
+  most_recent      = trusted_signers
+  owners           = ["self"]
+
+  filter {
+    name   = "Name"
+    values = ["abdimalik-mao-eng53-nodejs-*"]
+  }
+}
+
+data "aws_ami" "db" {
+  executable_users = ["self"]
+  most_recent      = trusted_signers
+  owners           = ["self"]
+
+  filter {
+    name   = "Name"
+    values = ["abdimalik-mao-eng53-db-*"]
+  }
+}
+
+
 module "elk" {
   source                  = "./elk"
   security_groups         = ["${module.vpc.security_group_elk}"]
@@ -34,7 +57,7 @@ module "db" {
   # my_public_key         = "C:/Users/aws.pub"
   security_group        = "${module.vpc.security_group}"
   private_subnets       = "${module.vpc.private_subnets}"
-  db_ami_id             = "ami-01793b684af7a3e2c" #DB image
+  db_ami_id             = "${data.aws_ami.db.id}" #DB image
   user_data_pr          = "${data.template_file.db_init.rendered}"
   user_data_sd          = "${data.template_file.db2_init.rendered}"
 
@@ -48,7 +71,7 @@ data "template_file" "app_init" {
 module "Autoscaling" {
   source            = "./Autoscaling"
   instance_type     = "t2.micro"
-  app_ami_id        = "ami-02e24fbcca656fe37"
+  app_ami_id        = "${data.aws_ami.app.id}"
   aws_vpc_id        = "${module.vpc.aws_vpc_id}"
   subnets           = "${module.vpc.subnets}"
   user_data_app     = "${data.template_file.app_init.rendered}"
